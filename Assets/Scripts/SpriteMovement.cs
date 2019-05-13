8bdd80e101c047fc87e809b068a23226b69986d7
Jonatan Karlsson <JK98@Live.se>
2019-05-13 11:21:03 +0200

    Initial commit

---
+++ Assets/Scripts/SpriteMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMovement : MonoBehaviour
+{
+    public float horizontalSpeed = 10.0f;
+    public float verticalSpeed = 100.0f;
+
+    
+    void Update()
+    {
+
+        float InputX = Input.GetAxis("Horizontal") * horizontalSpeed;
+        float InputY = Input.GetAxis("Vertical") * verticalSpeed;
+
+       
+    }
+}
