98d44fb7e9dc9baa2bb9c2818a3e53dd5072285b
Jonatan Karlsson <JK98@Live.se>
2019-05-13 11:35:25 +0200

    FirstCommit

--- Assets/Scripts/SpriteMovement.cs
+++ Assets/Scripts/SpriteMovement.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class SpriteMovement : MonoBehaviour
 {
+    private Rigidbody2D avgnRB;
+
     public float horizontalSpeed = 10.0f;
     public float verticalSpeed = 100.0f;
 
-    
-    void Update()
+    void Start()
+    {
+        avgnRB = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
     {
 
-        float InputX = Input.GetAxis("Horizontal") * horizontalSpeed;
-        float InputY = Input.GetAxis("Vertical") * verticalSpeed;
+        float InputX = Input.GetAxis("Horizontal");
+        float InputY = Input.GetAxis("Vertical");
 
-       
+        Vector2 movement = new Vector2(InputX, InputY);
+
+        avgnRB.AddForce(movement * horizontalSpeed);
     }
 }
